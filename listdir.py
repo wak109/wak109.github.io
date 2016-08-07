8045cb95624b4cc847de97a3e3c60e4682393955
U-ERICSSON\nrjryka <wak109@gmail.com>
2016-08-07 10:46:49 +0900

    Updated listdir.py

--- listdir.py
+++ listdir.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # vim: set ts=4 et sw=4 sts=4 fileencoding=utf-8:
 
+import argparse
 import os
 import sys
 import xml.etree.ElementTree as ET
+
+DEFAULT_TARGET_DIR = '.'
+DEFAULT_OUTPUT_FILE = sys.stdout
+DEFAULT_XSL = 'xsl/html5-hello-world.xsl'
+
 
 def create_xsl_root(xsl):
     root = ET.Element(None)
@@ -11,42 +17,40 @@ def create_xsl_root(xsl):
     return root
 
 
-def create_listdir_xml(target_dir):
-    return ET.Element('data')
+def create_listdir_xml(parent, target_dir):
+    return parent
 
 
 def create_xml_doc(target_dir, xsl):
     root = create_xsl_root(xsl)
-    root.append(create_listdir_xml(target_dir))
+    root.append(create_listdir_xml(ET.Element('top'), target_dir))
     return root
 
+
+def create_argparser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('dir',
+            type=str, default=DEFAULT_TARGET_DIR,
+            nargs='?',
+            metavar='<target-directory>',
+            help='Target Directory')
+    parser.add_argument('-o',
+            dest='output',
+            default=DEFAULT_OUTPUT_FILE,
+            metavar='<output-file>',
+            help='Target Directory')
+    parser.add_argument('-x',
+            dest='xsl',
+            default=DEFAULT_XSL,
+            metavar='<xsl-file>',
+            help='XSL file')
+    return parser
 
 ####################################################################
 # Main
 ####################################################################
 
 if __name__ == '__main__':
-
-    import argparse
-
-    def create_argparser():
-        parser = argparse.ArgumentParser(description=__doc__)
-        parser.add_argument('dir',
-                type=str, default='.', nargs='?',
-                metavar='<target-directory>',
-                help='Target Directory')
-        parser.add_argument('-o',
-                dest='output',
-                default=sys.stdout,
-                metavar='<output-file>',
-                help='Target Directory')
-        parser.add_argument('-x',
-                dest='xsl',
-                default='xsl/html5-hello-world.xsl',
-                metavar='<xsl-file>',
-                help='XSL file')
-        return parser
-
 
     args = create_argparser().parse_args()
 
