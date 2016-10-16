5c77df4d8f3751bf62331aca83164911634cc22a
U-ERICSSON\nrjryka <wak109@gmail.com>
2016-10-17 02:01:35 +0900

    hehe

--- cache.js
+++ cache.js
@@ -1,6 +1,6 @@
 // vim: set ts=4 et sw=4 sts=4 fileencoding=utf-8:
 
-const CACHE_NAME = "hehe1";
+const CACHE_NAME = "web_cache_v1";
 const URLS_TO_CACHE = [
     './index.git',
     './readme.md'
@@ -43,37 +43,40 @@ self.addEventListener('fetch', (event) => {
     console.log(event.request.url);
 
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
+        caches
+        .match(event.request)
+        .then((response) => {
 
-                // Hit the cache
-                if (response) {
-                    console.log("Found " + event.request.url + " in Cache");
-                    return response;
-                }
+            // Hit the cache
+            if (response) {
+                console.log("Found " + event.request.url + " in Cache");
+                return response;
+            }
 
-                // Clone the request because request is Stream
-                let fetchRequest = event.request.clone();
+            // Clone the request because request is Stream
+            let fetchRequest = event.request.clone();
 
-                return fetch(fetchRequest)
-                    .then((response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            console.log("Failed to fetch" + event.request.url);
-                            return response;
-                        }
-
-                        // Clone the response because response is Stream
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then((cache) => {
-                                console.log("Save " + event.request.url + " to cache");
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        console.log('Return response');
+            return
+                fetch(fetchRequest)
+                .then((response) => {
+                    if (!response || response.status !== 200 ||
+                        response.type !== 'basic') {
+                        console.log("Failed to fetch" + event.request.url);
                         return response;
+                    }
+
+                    // Clone the response because response is Stream
+                    let responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        console.log("Save " + event.request.url + " to cache");
+                        cache.put(event.request, responseToCache);
                     });
+
+                    console.log('Return response');
+                    return response;
+                });
             })
     );
 });
